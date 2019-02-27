7bd180aa49a8074bfb4e56632cb32b708cb4a098
thetruemzungu <latimer.ethan@gmail.com>
2019-02-27 15:12:34 -0500

    adding in vuex-persist to ensure that we don't lose information on refresh

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersist({
+  storage: window.sessionStorage
+})
+
 export default new Vuex.Store({
+  plugins: [vuexLocal.plugin],
   state: {
     drinks:  [{name: 'Kill Cliff', price: 2.00},{name: 'O2', price: 2.25},{name: 'StarBucks', price: 2.50},{name: 'Red Bull', price: 2.75},{name: 'Coke Zero', price: 3.00}],
     customers: [{firstName: 'ethan', middleName: 'thomas', lastName: 'latimer', initials: 'etl'}],
