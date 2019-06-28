a1fbc95a83bdb7419c5af375dc5d6ee9a848eea3
Ethan Latimer <ethan_t_latimer@homedepot.com>
2019-06-28 16:27:45 -0400

    floooooaat

--- src/service/api/api.service.js
+++ src/service/api/api.service.js
@@ -4,6 +4,7 @@ export default class ApiService {
     constructor() {
       const headers = this.setHeaders()
       console.log(process.env)
+      console.log(process.env.VUE_ENV_accountId)
       this.api = axios.create({
         baseURL: 'https://api.airtable.com/v0/' + process.env.accountId + '/',
         headers: headers
